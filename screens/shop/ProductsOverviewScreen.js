dc3da81693a1c4a427bb971087ad9bd250dcc51f
Shubham Singh <Shubham11.singh@paytm.com>
2020-02-13 19:09:39 +0530

    Cart Item added and total price updated

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { FlatList, StyleSheet, Text } from 'react-native';
+import { FlatList, StyleSheet, Text, Platform } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import ProductItem from '../../components/shop/ProductItem';
 import * as CartAction from '../../store/actions/cart';
+import CustomHeaderButton from '../../components/UI/HeaderButton';
 
 const ProductOverviewScreen = props => {
 
@@ -27,8 +29,19 @@ const ProductOverviewScreen = props => {
         )}/>)
 }
 
-ProductOverviewScreen.navigationOptions = {
-    headerTitle: 'All Products'
+ProductOverviewScreen.navigationOptions = navData => {
+    return {
+        headerTitle: 'All Products',
+        headerRight: () => ( 
+            <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+                <Item 
+                    title="Cart"
+                    iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
+                    onPress={() => {
+                        navData.navigation.navigate('Cart')
+                    }} />
+            </HeaderButtons>)
+        };
 }
 
 const styles = StyleSheet.create({
